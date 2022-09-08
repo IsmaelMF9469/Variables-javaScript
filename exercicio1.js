0dae9be89ca7d2d5ccacf85e9189d7fd0f2f430b
IsmaelMF9469 <ismaelmelo9469@hotmail.com>
2022-09-08 17:00:02 -0300

    Variables

---
+++ exercicio1.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+
+const p = prompt('Diga o seu peso: ');
+
+const name = prompt('Diga o seu nome: ');
+
+const alt = prompt('Diga a sua altura: ');
+
+const dataI = prompt('Diga a data da sua internação: ');
+
+const diab = prompt('O(a) senhor(a) é diabético? ');
